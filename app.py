5e854e492daae84d7ac15ea4ff82dd11827b4b85
Your Name <you@example.com>
2023-12-26 09:06:36 +0100

    minor changes

--- app.py
+++ app.py
@@ -344,7 +344,7 @@ def logout():
 
     current_user = cursor.fetchone()
     cursor.close()
-    flash(f"Tak for nu {current_user[1]}. Håber vi ses igen snart. ", "success")
+    flash(f"Tak for nu {current_user[1]}. Vi ses igen snart. ", "success")
     return redirect(url_for("login"))
 
 
