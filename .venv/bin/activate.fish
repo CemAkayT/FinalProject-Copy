effbdce596ea0b412f2ff981fcc6ef9fd36c8cca
OldRepo <cema0092@stud.kea.dk>
2024-01-18 21:23:49 +0100

    pip freeze showed empty. fixed now

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/cemakay/Desktop/KEAHOP/FinalProject/.venv"
+set -gx VIRTUAL_ENV "/Users/cemakay/Flask/FinalProject copy/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
