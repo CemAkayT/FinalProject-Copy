d545680fac7453c1f0406fc653964fee6c73db57
Cem Akay Türker <cemakay@Mac15.local>
2024-07-02 20:32:23 +0200

    Updated the requirements.txt file

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/cemakay/FinalProject/FinalProject-Copy/.venv"
+set -gx VIRTUAL_ENV "/Users/cemakay/Final Project/Final Project/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
